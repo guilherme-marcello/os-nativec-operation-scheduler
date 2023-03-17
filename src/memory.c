b593c9dc1b287e5ca56299e45a01d06f034cd08a
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2023-03-17 10:32:06 +0000

    Updated memory utils: added read/write to buffers

--- src/memory.c
+++ src/memory.c
@@ -1,4 +1,5 @@
 #include "../include/memory.h"
+#include "../include/memory-private.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,15 +17,26 @@ void destroy_dynamic_memory(void* ptr) {
     free(ptr);
 }
 
+void destroy_shared_memory(char* name, void* ptr, int size) {
+    // generate access key
+    key_t access_key = ftok(name, 'R');
+    // get the id of the shm segment
+    int shmid = shmget(access_key, size, 0666);
+    // detach shmem
+    shmdt(ptr);
+    // remove shmem
+    shmctl(shmid, IPC_RMID, NULL);
+}
+
 void* create_shared_memory(char* name, int size) {
     // generate access key as namekey concatenation
-    uid_t uid = getuid();
-    char key_as_string[64];
-    sprintf(key_as_string, "%s%d", name, uid);
-    key_t access_key = ftok(key_as_string, 'R');
+    //uid_t uid = getuid();
+    //char key_as_string[64];
+    //sprintf(key_as_string, "%s%d", name, uid);
+    key_t access_key = ftok(name, 'R');
 
     // get the id of the shm segment
-    int shmid = shmget(access_key, size, 0644|IPC_CREAT);
+    int shmid = shmget(access_key, size, 0666|IPC_CREAT);
     // attach to the shm segment, getting a pointer
     void* ptr = shmat(shmid, NULL, 0);
     // fill segment with 0
@@ -36,12 +48,56 @@ void* create_shared_memory(char* name, int size) {
 
 void write_main_client_buffer(struct rnd_access_buffer* buffer, int buffer_size, struct operation* op) {
     for (int i = 0; i < buffer_size; i++) {
-        // check if the current position in the buffer is free
-        if (buffer->ptrs[i] == 0) {
+        // check if the ith position in the buffer is free
+        if (buffer->ptrs[i] == FREE_MEM) {
             // write the operation to the buffer and update pointer to 1 (used)
             buffer->buffer[i] = *op;
-            buffer->ptrs[i] = 1;
+            buffer->ptrs[i] = USED_MEM;
             return; // exit the function
         }
     }
+}
+
+void read_main_client_buffer(struct rnd_access_buffer* buffer, int client_id, int buffer_size, struct operation* op) {
+    for (int i = 0; i < buffer_size; i++) {
+        // check if the ith position in the buffer is in used
+        if (buffer->ptrs[i] == USED_MEM) {
+            if (buffer->buffer[i].requesting_client == client_id) {
+                // point op to buffered op and update ith position to free
+                op = &buffer->buffer[i];
+                buffer->ptrs[i] = FREE_MEM;
+                return; // exit the function
+            }
+        }
+    }
+    // no op available
+    op->id = -1;
+}
+
+void write_interm_enterp_buffer(struct rnd_access_buffer* buffer, int buffer_size, struct operation* op) {
+    for (int i = 0; i < buffer_size; i++) {
+        // check if the ith position in the buffer is free
+        if (buffer->ptrs[i] == FREE_MEM) {
+            // write the operation to the buffer and update pointer to 1 (used)
+            buffer->buffer[i] = *op;
+            buffer->ptrs[i] = USED_MEM;
+            return; // exit the function
+        }
+    }    
+}
+
+void read_interm_enterp_buffer(struct rnd_access_buffer* buffer, int enterp_id, int buffer_size, struct operation* op) {
+    for (int i = 0; i < buffer_size; i++) {
+        // check if the ith position in the buffer is in used
+        if (buffer->ptrs[i] == USED_MEM) {
+            if (buffer->buffer[i].requested_enterp == enterp_id) {
+                // point op to buffered op and update ith position to free
+                op = &buffer->buffer[i];
+                buffer->ptrs[i] = FREE_MEM;
+                return; // exit the function
+            }
+        }
+    }
+    // no op available
+    op->id = -1;
 }
