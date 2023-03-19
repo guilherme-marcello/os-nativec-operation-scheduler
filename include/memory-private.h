3c65cd26fa24d85874ed8b1790fb854da580e469
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2023-03-19 20:05:36 +0000

    Added error messages for SHMEM creation

--- include/memory-private.h
+++ include/memory-private.h
@@ -1,2 +1,17 @@
+#ifndef MEMORY_PRIVATE_H_GUARD
+#define MEMORY_PRIVATE_H_GUARD
+
 #define FREE_MEM 0
 #define USED_MEM 1
+
+// Error handling constants
+#define ERROR_SHM_OPEN "Error: Failed to open shared memory region.\n"
+#define ERROR_SHM_TRUNCATE "Error: Failed to truncate shared memory region.\n"
+#define ERROR_SHM_MAP "Error: Failed to map shared memory region.\n"
+#define ERROR_SHM_UNMAP "Error: Failed to unmap shared memory region.\n"
+#define ERROR_SHM_UNLINK "Error: Failed to unlink shared memory region.\n"
+
+#define EXIT_MUNMAP_ERROR 7
+#define EXIT_SHM_UNLINK_ERROR 8
+
+#endif
